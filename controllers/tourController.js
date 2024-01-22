ac4485206f851bd784cefdb80bde7d7328166101
arvinder katoch <arvinderkatoch02@gmail.com>
2024-01-22 23:31:23 +0530

    Error Handling 114(Pending)

---
+++ controllers/tourController.js
@@ -0,0 +1,186 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line node/no-extraneous-require
+const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = 'ratingAverage,price';
+  req.query.fields = 'name,price,ratingAverage,summary,difficulty';
+  next();
+};
+
+exports.getAllTours = async (req, res) => {
+  try {
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
+
+    //Send response
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err.message
+    });
+  }
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failure',
+      message: err.message
+    });
+  }
+};
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingAverage: { $gte: 4.5 } }
+      },
+
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingQuantity' },
+          avgRating: { $avg: '$ratingAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      }
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
+};
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: [err, err.message]
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // returns the modified document after update
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: [err, err.message]
+    });
+  }
+};
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success'
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: [err, err.message]
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDate'
+      },
+      {
+        $match: {
+          startDate: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDate' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      {
+        $addFields: { month: '$_id' }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: { numTourStarts: -1 }
+      }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { plan }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: [err, err.message]
+    });
+  }
+};
---
+++ models/tourModel.js
@@ -0,0 +1,106 @@
+const validator = require('validator');
+
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxLength: [40, 'A tour must have less or equal then 40 character '],
+      minlength: [10, 'A tour must have more or equal than 10 characters'],
+      validate: [validator.isAlpha, 'Tour name must only contain character']
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+
+    maxGroupSize: {
+      type: Number,
+
+      required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Diffculty either easy,medium,diffculy'
+      }
+    },
+
+    ratingAverage: {
+      type: Number,
+      default: 0,
+      min: [1, 'Rating must br above 1.0'],
+      max: [1, 'Rating must br above 1.0']
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          // this only points to current doc on new document creation
+
+          // this will not work for update since it will refer to the original document but not the updated value we are sending during update
+          console.log(this.price);
+          return this.price > val;
+        },
+        message: 'Discount price ({VALUE}) should be less than regular price'
+      },
+      summary: {
+        type: String,
+        trim: true
+      },
+      description: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have description']
+      },
+
+      imageCover: {
+        type: String,
+        required: [true, 'A tour must have cour image']
+      },
+      image: [String],
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false
+      },
+      startDate: [Date],
+      secretTour: {
+        type: Boolean,
+        default: false
+      }
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
+});
+//run before .save() and .create()
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
